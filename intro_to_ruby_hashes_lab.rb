cec0db9da8719b1ff4d9fa9b6232f26fa8dc203e
Eric Cave <caveman4949@gmail.com>
2019-10-25 19:53:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,3 +24,26 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
+
+countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    },
+    "USA" => {
+      :capital => "Washington D.C.",
+      :capital_climate => "Kőppen Cfa"
+    }
+  },
+  :africa => {
+    "Ghana" => {
+      :capital => "Accra",
+      :capital_climate => "Kőppen Aw"
+    },
+    "Nigeria" => {
+      :capital => "Abuja",
+      :capital_climate => "Kőppen Aw"
+    }
+  }
+}
